e17e573fbf5ecd4bc6bf806bc81755a2142a5287
Aritra Manna <161790604+Aritrama12@users.noreply.github.com>
2024-10-17 19:19:26 -0700

    Adding html and css file.

---
+++ script.js
@@ -0,0 +1,10 @@
+ menuIcon = document.getElementById("menu-icon");
+ menu = document.getElementById("menu");
+menuIcon.addEventListener("click",() => {
+    if(menu.className == "hidden"){
+        menu.classList.remove("hidden");
+    }
+    else{
+        menu.classList.add("hidden");
+    }
+});
